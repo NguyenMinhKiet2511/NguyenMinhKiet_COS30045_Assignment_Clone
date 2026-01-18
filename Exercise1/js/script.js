8dc3249f53171d595d21f5d962a3dfcd5c0567d6
NguyenMinhKiet2511 <nguyenminhkiet25110202@gmail.com>
2026-01-18 11:51:45 +0700

    Complete Exercise 0.2

--- Exercise1/js/script.js
+++ Exercise1/js/script.js
@@ -1,4 +1,44 @@
-// In dòng này ra màn hình Console của trình duyệt để kiểm tra kết nối
-console.log("Javascript đã được kết nối thành công!");
+const btnHome = document.getElementById('btn-home');
+const btnTv = document.getElementById('btn-tv');
+const btnAbout = document.getElementById('btn-about');
 
-// Code chuyển trang sẽ viết ở đây sau
+
+const sectionHome = document.getElementById('home');
+const sectionTv = document.getElementById('televisions');
+const sectionAbout = document.getElementById('about');
+
+function switchPage(activeSection, activeBtn) {
+    
+    sectionHome.classList.add('hidden');
+    sectionTv.classList.add('hidden');
+    sectionAbout.classList.add('hidden');
+
+    btnHome.classList.remove('active');
+    btnTv.classList.remove('active');
+    btnAbout.classList.remove('active');
+
+    activeSection.classList.remove('hidden');
+
+    activeBtn.classList.add('active');
+}
+
+btnHome.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    switchPage(sectionHome, btnHome); 
+});
+
+btnTv.addEventListener('click', function() {
+    event.preventDefault();
+    switchPage(sectionTv, btnTv);
+});
+
+btnAbout.addEventListener('click', function() {
+    event.preventDefault();
+    switchPage(sectionAbout, btnAbout);
+});
+
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', function() {
+    switchPage(sectionHome, btnHome);
+});
